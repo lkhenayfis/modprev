09761de384ce0e22ae7ac0c4050e4988d3282a05
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2021-12-10 17:12:26 -0300

    Add script titulo do pacote

---
+++ R/modprev.r
@@ -0,0 +1,11 @@
+#' Interface Unificada Para Modelagem
+#' 
+#' @docType package
+#' @name modprev
+#' 
+#' @useDynLib modprev
+#' 
+#' @import data.table
+#' @import forecast
+#' @import KFAS
+NULL
