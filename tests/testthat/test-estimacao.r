3c6c64506dc76c212ea4f089563edbe55e08b6c2
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2021-12-10 17:17:09 -0300

    Adiciona teste de falta de regdata em reg_din

--- tests/testthat/test-estimacao.r
+++ tests/testthat/test-estimacao.r
@@ -16,17 +16,19 @@ test_that("Estimacao de modelo S.S. AR1+Saz", {
     expect_equal(mod$modelo["T"]["custom2", "custom2", 1], 0.9957003)
 })
 
-test_that("Estimacao de modelo S.S. RegDin", {
+test_that("Estimacao de modelo S.S. RegDin - regressao simples", {
     serie <- window(datregdin[[1]], 1, 200)
-    varex <- window(datregdin[[2]], 1, 200)
+    varex <- data.frame(venprev = window(datregdin[[2]], 1, 200))
     mod   <- estimamodelo(serie, regdata = varex, tipo = "ss_reg")
 
     expect_equal("ss_reg_din", attr(mod, "tipo"))
     expect_equal(mod$modelo["Q"][1, 1, 1], 0.059735078) # precalculados e testados para garantir
     expect_equal(mod$modelo["H"][1, 1, 1], 0.67102834)
 
+    expect_error(estimamodelo(serie, tipo = "ss_reg"))
+
     # testa aviso quando tem NA na variavel explicativa
     serie <- datregdin[[1]]
-    varex <- datregdin[[2]]
+    varex <- data.frame(venprev = datregdin[[2]])
     expect_warning(estimamodelo(serie, regdata = varex, tipo = "ss_reg"))
 })
