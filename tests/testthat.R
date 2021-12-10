bd8fb85e719a2336a1514869eb7741f9273368de
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2021-12-10 17:12:04 -0300

    Adiciona testes ao pacote

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(modprev)
+
+test_check("modprev")
---
+++ tests/testthat/test-estimacao.r
@@ -0,0 +1,32 @@
+
+test_that("Estimacao de modelo ARIMA", {
+    compmod <- forecast::auto.arima(AirPassengers)
+    mod     <- estimamodelo(AirPassengers, tipo = "sarima")
+
+    expect_equal("sarima", attr(mod, "tipo"))
+    expect_equal(coef(compmod), coef(mod$model))
+})
+
+test_that("Estimacao de modelo S.S. AR1+Saz", {
+    mod <- estimamodelo(AirPassengers, tipo = "ss_ar1_saz")
+
+    expect_equal("ss_ar1_saz", attr(mod, "tipo"))
+    expect_equal(mod$modelo["Q"][1, 1, 1], 17.183724) # precalculados e testados para garantir
+    expect_equal(mod$modelo["Q"][2, 2, 1], 172.71706)
+    expect_equal(mod$modelo["T"]["custom2", "custom2", 1], 0.9957003)
+})
+
+test_that("Estimacao de modelo S.S. RegDin", {
+    serie <- window(datregdin[[1]], 1, 200)
+    varex <- window(datregdin[[2]], 1, 200)
+    mod   <- estimamodelo(serie, regdata = varex, tipo = "ss_reg")
+
+    expect_equal("ss_reg_din", attr(mod, "tipo"))
+    expect_equal(mod$modelo["Q"][1, 1, 1], 0.059735078) # precalculados e testados para garantir
+    expect_equal(mod$modelo["H"][1, 1, 1], 0.67102834)
+
+    # testa aviso quando tem NA na variavel explicativa
+    serie <- datregdin[[1]]
+    varex <- datregdin[[2]]
+    expect_warning(estimamodelo(serie, regdata = varex, tipo = "ss_reg"))
+})
