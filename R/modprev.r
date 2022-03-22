0fea46467b90c1a6b5d2eedc7679c89629ae82a1
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2022-03-22 18:25:51 -0300

    Explicita dependencias faltantes

--- R/modprev.r
+++ R/modprev.r
@@ -1,9 +1,14 @@
 #' Interface Unificada Para Modelagem
 #' 
+#' Pacote contendo um arcabouco unificado para estimacao e manipulacao de diversos tipos de modelos
+#' 
 #' @docType package
 #' @name modprev
 #' 
 #' @import data.table
 #' @import forecast
 #' @import KFAS
+#' 
+#' @importFrom graphics grid lines title
+#' @importFrom stats fitted frequency logLik predict start ts update window
 NULL
