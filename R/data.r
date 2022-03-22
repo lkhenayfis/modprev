32f5560f7983af669ea014e6bb559fdb36ccb8f7
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2022-03-22 18:21:36 -0300

    Documenta dado exemplo do pacote

---
+++ R/data.r
@@ -0,0 +1,11 @@
+#' Dado Exemplo De Velocidade Do Vento
+#' 
+#' Dado para exemplo de ajuste dos modelos, contendo informacoes de velocidade do vento prevista e
+#' observada
+#' 
+#' @format \code{data.frame} de duas colunas:
+#' \describe{
+#' \item{obs}{velocidade de vento observada}
+#' \item{prev}{velocidade de vento prevista}
+#' }
+"datregdin"
