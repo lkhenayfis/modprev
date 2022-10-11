5a634e6a64021b9d004cda5b11f51e92abf85072
Lucas de Souza Khenayfis <lucask@ons.org.br>
2022-10-11 11:36:56 -0300

    Add metodo predict p sarimax

--- R/mod-sarimax.r
+++ R/mod-sarimax.r
@@ -35,8 +35,48 @@ sarimax <- function(serie, regdata = NULL, formula = NULL) {
     Xreg <- model.frame(formula, data = regdata)
     Xreg <- data.matrix(Xreg)
 
-    mod <- auto.arima(serie, allowdrift = FALSE, xreg = Xreg)
-    out <- new_modprev(mod, serie, "sarimax")
+    mod <- auto.arima(serie, xreg = Xreg)
+
+    mod_atrs <- list(formula = formula)
+    out <- new_modprev(mod, serie, "sarimax", mod_atrs)
 
     return(out)
+}
+
+# METODOS ------------------------------------------------------------------------------------------
+
+#' @param object objeto com classes \code{c("sarimax", "modprev")} contendo modelo
+#' @param newdata \code{data.frame}-like contendo variéveis explicativas fora da amostra
+#' @param n.ahead número de passos à frente para previsão. Este argumento não é necessario, caso não
+#'     seja informado a previsão sera feita tantos passos à frente quanto amostras em \code{newdata}
+#' @param ... nao possui uso, existe apenas para consistencia com a generica
+#' 
+#' @return \code{predict} serie temporal multivariada contendo valor esperado e desvio padrao de
+#'     previsão \code{n.ahead} passos à frente;
+#' 
+#' @rdname modelos_sarimax
+#' 
+#' @export
+
+predict.sarimax <- function(object, newdata, n.ahead, ...) {
+    modelo <- object$modelo
+
+    if(missing(newdata)) stop("Forneca a variavel explicativa para previsao atraves do parametro newdata")
+
+    if(!missing(n.ahead)) {
+        regobs <- min(n.ahead, nrow(newdata))
+        newdata <- newdata[seq(regobs), , drop = FALSE]
+    }
+
+    formula <- attr(object, "mod_atrs")$formula
+    Xreg    <- model.frame(formula, data = newdata)
+    Xreg    <- data.matrix(Xreg)
+
+    prev   <- forecast(modelo, xreg = Xreg, level = c(.95), ...)
+    prevsd <- with(prev, mean - lower) / qnorm(.975)
+
+    prev <- cbind(prev$mean, prevsd)
+    colnames(prev) <- c("prev", "sd")
+
+    return(prev)
 }
