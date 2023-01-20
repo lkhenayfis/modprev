d0f5bb0e9f8541cd14d23f694b950d6223822497
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2023-01-20 18:41:33 -0300

    Add testes dcsregdin multivariada

--- tests/testthat/test-dcs_reg_din.r
+++ tests/testthat/test-dcs_reg_din.r
@@ -1,7 +1,7 @@
 
 test_that("Estimacao de modelo DCS RegDin - regressao simples", {
-    serie <- window(datregdin$obs, 1, 200)
-    varex <- datregdin$varex[, "V1", drop = FALSE]
+    serie <- window(datregdin$obs, 1, 100)
+    varex <- datregdin$varex[1:100, "V1", drop = FALSE]
     mod   <- estimamodelo(serie, regdata = varex, tipo = "dcs_reg_din", formula = ~ V1)
 
     expect_equal("dcs_reg_din", class(mod)[1])
@@ -78,4 +78,81 @@ test_that("Atualizacao de modelo DCS RegDin - regressao simples", {
 
     expect_equal(mod_atr_upd$formula, mod_atr$formula)
     expect_equal(mod_atr_upd$vardin, mod_atr$vardin)
+})
+
+test_that("Estimacao de modelo DCS RegDin - regressao multipla", {
+
+    serie <- window(datregdin$obs, 1, 100)
+    varex <- datregdin$varex[1:100, ]
+
+    mod <- estimamodelo(serie, "dcs_reg_din", regdata = varex, formula = ~ V1 + V2 * V3)
+
+    expect_equal("dcs_reg_din", class(mod)[1])
+    expect_snapshot_value(round(coef(mod$modelo), 5), style = "deparse")
+
+    serie <- c(serie)
+    mod   <- estimamodelo(serie, formula = ~ V1 + V2 * V3, regdata = varex, tipo = "dcs_reg_din")
+
+    expect_error(estimamodelo(serie, formula = ~ V1 + V2 * V3, tipo = "dcs_reg_din"))
+
+    varex_cna <- varex
+    varex_cna[c(10, 20, 30), ] <- NA_real_
+    expect_warning(estimamodelo(serie, formula = ~ V1 + V2 * V3, regdata = varex_cna, tipo = "dcs_reg_din"))
+})
+
+test_that("Previsao de modelo DCS RegDin - regressao multipla", {
+
+    serie <- window(datregdin$obs, 1, 150)
+    varex <- datregdin$varex[1:150, ]
+
+    mod <- estimamodelo(serie, formula = ~ V1 + V2 * V3, regdata = varex, tipo = "dcs_reg_din")
+
+    newdata <- datregdin$varex[151:170, ]
+    prev    <- predict(mod, newdata = newdata)
+
+    expect_true(all(dim(prev) == c(20, 2)))
+    expect_equal(c("prev", "sd"), colnames(prev))
+
+    expect_snapshot_value(round(prev[1, 1], 5), style = "deparse")
+
+    prev <- predict(mod, newdata = newdata, n.ahead = 10)
+
+    expect_true(all(dim(prev) == c(10, 2)))
+    expect_equal(c("prev", "sd"), colnames(prev))
+
+    expect_snapshot_value(round(prev[1, 1], 5), style = "deparse")
+
+    expect_error(predict(mod))
+})
+
+test_that("Atualizacao de modelo DCS RegDin - regressao multipla", {
+
+    serie <- window(datregdin$obs, 1, 150)
+    varex <- datregdin$varex[1:150, ]
+
+    mod <- estimamodelo(serie, formula = ~ V1 + V2 * V3, regdata = varex, tipo = "dcs_reg_din")
+
+    newseries  <- window(datregdin$obs, 151, 200)
+    newregdata <- datregdin$varex[151:200, ]
+    mod_upd   <- update(mod, newseries = newseries, newregdata = newregdata)
+
+    expect_equal(coef(mod$modelo), coef(mod_upd$modelo))
+    expect_true(all(mod_upd$modelo["Z"]["mu", 2:4, ] - t(data.matrix(newregdata)) == 0))
+    expect_true(all(mod_upd$serie - newseries == 0))
+
+    mod_atr     <- attr(mod, "mod_atrs")
+    mod_atr_upd <- attr(mod_upd, "mod_atrs")
+
+    expect_equal(mod_atr_upd$formula, mod_atr$formula)
+    expect_equal(mod_atr_upd$vardin, mod_atr$vardin)
+
+    mod_refit <- update(mod, newseries = newseries, newregdata = newregdata, refit = TRUE)
+
+    expect_true(all(mod_upd$modelo["Z"]["mu", 2:4, ] - t(data.matrix(newregdata)) == 0))
+    expect_true(all(mod_upd$serie - newseries == 0))
+    expect_snapshot_value(round(coef(mod$modelo), 5), style = "deparse")
+
+    mod_atr_refit <- attr(mod_refit, "mod_atrs")
+    expect_equal(mod_atr_refit$formula, mod_atr$formula)
+    expect_equal(mod_atr_refit$vardin, mod_atr$vardin)
 })
