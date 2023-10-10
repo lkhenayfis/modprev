8eddaeb49715fb11b111cd00ff76473583a60a4f
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2023-10-10 10:42:24 -0300

    Add predict e update de modelos GAM

--- R/mod-gam.r
+++ R/mod-gam.r
@@ -38,7 +38,7 @@ NULL
 #'     internamente
 #' @param ... nao possui uso, existe apenas para consistencia com a generica
 #' 
-#' @return Objeto da classe \code{modprev} e subclasse \code{gam}, uma lista de dois 
+#' @return Objeto da classe \code{modprev} e subclasse \code{GAM}, uma lista de dois 
 #'     elementos: \code{modelo} e \code{serie} contendo o modelo estimado e a série passada
 #' 
 #' @rdname modelos_gam
@@ -61,4 +61,84 @@ GAM <- function(serie, regdata, formula, pesos = rep(1, length(serie)), ...) {
     mod_atrs <- list(formula = formula, tsp = aux_tsp, pesos = pesos)
 
     new_modprevU(fit, serie, "GAM", mod_atrs)
+}
+
+# METODOS ------------------------------------------------------------------------------------------
+
+#' \bold{Predict}:
+#' 
+#' @param object objeto com classes \code{c("GAM", "modprev")} contendo modelo
+#' @param newdata \code{data.frame}-like contendo variéveis explicativas fora da amostra
+#' @param n.ahead número de passos à frente para previsão. Este argumento não é necessario, caso não
+#'     seja informado a previsão sera feita tantos passos à frente quanto amostras em \code{newdata}
+#' @param ... nao possui uso, existe apenas para consistencia com a generica
+#' 
+#' @return \code{predict} serie temporal multivariada contendo valor esperado e desvio padrao de
+#'     previsão \code{n.ahead} passos à frente;
+#' 
+#' @rdname modelos_gam
+#' 
+#' @export
+
+predict.GAM <- function(object, newdata, n.ahead, ...) {
+    modelo <- object$modelo
+    aux_tsp <- attr(object, "mod_atrs")$tsp
+
+    if (missing(newdata)) stop("Forneca a variavel explicativa para previsao atraves do parametro 'newdata'")
+
+    if (!missing(n.ahead)) {
+        regobs <- min(n.ahead, nrow(newdata))
+        newdata <- newdata[seq(regobs), , drop = FALSE]
+    }
+
+    prev <- predict(modelo, newdata = newdata, se.fit = TRUE)
+    prev <- do.call(cbind, prev[1:2])
+    colnames(prev) <- c("prev", "sd")
+
+    prox_t <- aux_tsp[2] + 1 / aux_tsp[3]
+    prev <- ts(prev, start = prox_t, frequency = aux_tsp[3])
+
+    return(prev)
+}
+
+#' @details 
+#' 
+#' @param newseries nova série com a qual atualizar o modelo
+#' @param newregdata \code{data.frame}-like contendo variáveis explicativas na nova amostra
+#' @param refit booleano indicando se o modelo deve ou nao ser reajustado
+#' @param ... nao possui uso, existe apenas para consistencia com a generica
+#' 
+#' @return \code{update} retorna modelo com novos dados e, caso \code{refit == TRUE}, reajustado. 
+#'     Contrário à função de estimação, \code{update} já retorna o objeto da classe \code{modprev};
+#' 
+#' @rdname modelos_gam
+#' 
+#' @export
+
+update.GAM <- function(object, newseries, newregdata, refit = FALSE, ...) {
+
+    mod_atrs <- attr(object, "mod_atrs")
+
+    if (refit) {
+        pesos   <- mod_atrs$pesos[seq_along(newseries)]
+        formula <- mod_atrs$formula
+        object  <- estimamodelo(newseries, "GAM", regdata = newregdata, formula = formula,
+            pesos = pesos)
+    } else {
+
+        modelo <- object$modelo
+
+        if (missing(newregdata)) stop("Forneca nova variavel explicativa atraves do parametro 'newregdata'")
+
+        modelo$y <- as.numeric(newseries)
+        modelo$model <- cbind.data.frame(Y = as.numeric(newseries), newregdata)
+        modelo$fitted.values <- predict(modelo)
+        modelo$residuals <- as.numeric(newseries) - fitted(modelo)
+
+        mod_atrs$tsp <- tsp(newseries)
+
+        object <- new_modprevU(modelo, newseries, "GAM", mod_atrs)
+    }
+
+    return(object)
 }
